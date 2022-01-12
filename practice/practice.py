f37c2e4002000770bb40242d3bf9ebf375ae1ce0
Sean Montgomery <seandmontgomery@gmail.com>
2022-01-12 11:02:59 -0500

    create pracitce file

---
+++ practice/practice.py
@@ -0,0 +1,49 @@
+# character_name = "Jim"
+# character_age = "50"
+# print("There once was a man named " + character_name + ", ")
+# print("he was " +character_age+ " years old. ")
+
+# character_name = "Sean"
+# print("He really liked the name " + character_name + ", ")
+# print("but didn't like being " + character_age + ".")
+
+
+# print("Python\nPractice")
+# print("Python\"Practice")
+# print("Python\Practice")
+
+# phrase = "Python Practice"
+# print(phrase)
+
+# phrase = "Python Practice"
+# print(phrase + " is fun")
+
+# phrase = "Python Practice"
+# print(phrase.lower())
+
+# phrase = "Python Practice"
+# print(phrase.upper())
+
+# phrase = "Python Practice"
+# print(phrase.isupper())
+
+# phrase = "Python Practice"
+# print(phrase.upper().isupper())
+
+# phrase = "Python Practice"
+# print(len(phrase))
+
+# phrase = "Python Practice"
+# print(phrase[0])
+
+# phrase = "Python Practice"
+# print(phrase.index("P"))
+
+# phrase = "Python Practice"
+# print(phrase.index("a"))
+
+# phrase = "Python Practice"
+# print(phrase.index("Prac"))
+
+# phrase = "Python Practice"
+# print(phrase.replace("Python", "Javascript"))
